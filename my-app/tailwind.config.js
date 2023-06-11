9fa721e936362d8791e1084f8c29ed521f35b7bb
Matt Nguyen <coose@Matts-MBP.attlocal.net>
2023-06-11 12:08:44 -0700

    working on hero component and modified photo

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'sm': {'min': '576px', 'max': '767px'},
+        'sm': {'max': '767px'},
         'md': {'min': '768px', 'max': '991px'},
         'lg': {'min': '992px', 'max': '1199px'},
         'xl': {'min': '1200px'},
