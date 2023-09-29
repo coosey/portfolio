04c1e2ba8be8fe0ff84bb7f3cee4582d098a74d2
Matthew Nguyen <mnguyen@777part.com>
2023-09-29 15:16:51 -0700

    created timeline component for experience section

--- my-app/src/components/timeline/index.jsx
+++ my-app/src/components/timeline/index.jsx
@@ -1,7 +1,6 @@
-
 export const Timeline = () => {
   return (
-    <section>
+    <section className="font-satoshi m-auto ml-5 mr-5 flex flex-col flex-wrap p-4 text-justify">
       <ol class="relative border-l border-gray-200 dark:border-gray-700">
         <li class="ml-4">
           <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white"></div>
@@ -9,19 +8,22 @@ export const Timeline = () => {
           <h3 class="text-2xl font-semibold text-gray-900">Frontend Developer</h3>
           <ul className="mx-3 leading-6 list-disc font-medium text-left text-xl text-gray-800">
             <li className="py-2">
-              Developed Bonza Airline’s Passenger Service System that is used by admins & travel agents to help book flights & manage
-              bookings for thousands of Australian customers
+              Developed a Passenger Service System (internal Ecommerce platform) for admins & travel agents to help book flights & manage
+              bookings for thousands of Australian customers which generated over 600k AUD within the first week of sales
             </li>
             <li className="py-2">
-              Implemented layered architecture that separates data and business logic to improve maintainability and create modular,
-              scalable React components for future reuse
+              Utilized a layered architecture to create separation of logic between UI and data layer to improve maintainability and create modular,
+              scalable React components for code reuse
             </li>
             <li className="py-2">
-              Integrated internal SDK with backend API to modify or allow specific functionality of booking/management flow
+              Integrated internal SDK to execute with backend API which in turn allows modification to specific functionality within booking/management flow
+              (i.e. pending payment, order retrieval, order change)
             </li>
             <li className="py-2">
-              Lead booking management & booking flow development process from concept to production while working alongside with
-              business analysts, product managers, and developers
+              Executed as one of the leading developer for the booking and management development's process from concept to production
+            </li>
+            <li className="py-2">
+              Updated current payment system to utilize Uplift's 3rd Party payment API throughout booking process
             </li>
           </ul>
         </li>
