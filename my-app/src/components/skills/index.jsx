b805c1ffadcdeaedce6c2f92d11df6e1eb943ef8
Matthew Nguyen <mnguyen@777part.com>
2023-10-04 15:04:36 -0700

    added title attribute with img

--- my-app/src/components/skills/index.jsx
+++ my-app/src/components/skills/index.jsx
@@ -7,7 +7,7 @@ export const Skills = () => {
       <SectionTitle title="Technologies" />
       <div className="flex flex-row flex-wrap px-10 py-10 gap-[4rem] justify-center items-center">
         {DEVICONS?.map?.((skill) => (
-          <img src={skill?.link} alt={skill?.name} loading="lazy" width="75px" height="75px" />
+          <img src={skill?.link} alt={skill?.name} title={skill?.name} loading="lazy" width="75px" height="75px" />
         ))}
       </div>
     </div>
