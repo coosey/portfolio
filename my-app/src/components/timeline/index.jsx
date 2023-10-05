4160d9121c667e70cfeeec142e83f22f7980ae9b
Matthew Nguyen <mnguyen@777part.com>
2023-10-05 14:43:31 -0700

    revised timeline experience with updated resume points

--- my-app/src/components/timeline/index.jsx
+++ my-app/src/components/timeline/index.jsx
@@ -1,29 +1,26 @@
 export const Timeline = () => {
   return (
     <section className="font-satoshi m-auto ml-5 mr-5 flex flex-col flex-wrap p-4 text-justify">
-      <ol class="relative border-l border-gray-200 dark:border-gray-700">
-        <li class="ml-4">
-          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white"></div>
-          <time class="mb-1 text-lg font-montserrat leading-none text-gray-900">March 2022 - present</time>
-          <h3 class="text-2xl font-semibold text-white">Frontend Developer</h3>
-          <ul className="mx-3 leading-6 list-disc font-rale font-light text-left text-xl text-black">
+      <ol className="relative border-l border-gray-200 dark:border-gray-700">
+        <li className="ml-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white"></div>
+          <time className="mb-1 text-lg font-montserrat leading-none text-gray-900">March 2022 - present</time>
+          <h3 className="text-2xl font-semibold text-white">Frontend Developer</h3>
+          <ul className="mx-3 list-disc font-rale font-normal text-left xs:text-md text-xl text-white">
             <li className="py-2">
-              Developed a Passenger Service System (internal Ecommerce platform) for admins & travel agents to help book flights & manage
-              bookings for thousands of Australian customers which generated over 600k AUD within the first week of sales
+              Devised and implemented a bespoke airline internal ecommerce travel platform for admins and agents to purchase flights and manage bookings, generating over AUD 800,000 in sales in its first week
             </li>
             <li className="py-2">
-              Utilized a layered architecture to create separation of logic between UI and data layer to improve maintainability and create modular,
-              scalable React components for code reuse
+              Designed and utilized a modular React architecture to decouple data and UI logic, improving maintainability and enabling scalable React components for code reuse
             </li>
             <li className="py-2">
-              Integrated internal SDK to execute with backend API which in turn allows modification to specific functionality within booking/management flow
-              (i.e. pending payment, order retrieval, order change)
+              Integrated frontend logic with SDKs to orchestrate interactions with Java APIs for flight inventory management, departure control, RSVPS, and payments
             </li>
             <li className="py-2">
-              Executed as one of the leading developer for the booking and management development's process from concept to production
+              Reengineered the booking and payment flow to accommodate a third-party payment API, enhancing user experience and simplifying the overall process
             </li>
             <li className="py-2">
-              Updated current payment system to utilize Uplift's 3rd Party payment API throughout booking process
+              Restructured legacy code to enhance scalability, readability, and performance, delivering a 7% improvement in overall speed
             </li>
           </ul>
         </li>
